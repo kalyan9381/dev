efde8df9802585b36d7737b7a9a6a6ec06080cfe
kalyan9381 <80843988+kalyan9381@users.noreply.github.com>
2021-03-29 23:26:44 +0530

    Update and rename src/main/java/com/maaven/practice/App.java to src/main/java/with/maaven/practice/App.java

--- src/main/java/with/maaven/practice/App.java
+++ src/main/java/with/maaven/practice/App.java
@@ -6,7 +6,7 @@ package com.maaven.practice;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main1( String[] args )
     {
         System.out.println( "Hello World!" );
     }
